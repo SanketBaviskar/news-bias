268e466bfd70a50fbce7d73f72d5841c128929d9
Sanket Baviskar <sanketbaviskar01@gmail.com>
2025-12-03 10:38:43 -0700

    chore: Update frontend dependencies.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,9 @@ function App() {
 
 		try {
 			const response = await axios.post(
-				import.meta.env.VITE_API_URL || "http://localhost:5000/predict",
+				`${
+					import.meta.env.VITE_API_URL || "http://localhost:5000"
+				}/predict`,
 				{ news_text: text },
 				{ headers: { "Content-Type": "multipart/form-data" } }
 			);
